747d465556d105d1f40ae9ec887f37c5cbc927db
szyang78 <89109677+szyang78@users.noreply.github.com>
2023-12-02 18:03:27 -0500

    Add files via upload
    updated reverse

--- Shizhe_code/reverse_rotation_engine_F.v
+++ Shizhe_code/reverse_rotation_engine_F.v
@@ -46,44 +46,41 @@ rst,
     
     initial 
     begin
-    r1_out <= r1_pos_F;
-    r2_out <= r2_pos_F;
-    r3_out <= r3_pos_F;
+    r1_out = r1_pos_F;
+    r2_out = r2_pos_F;
+    r3_out = r3_pos_F;
     end
     
     always@(rst)begin
     if(rst==1'd1)begin
-    r1_out<=5'd0;
-    r2_out<=5'd0;
-    r3_out<=5'd0;
+    r1_out=5'd0;
+    r2_out=5'd0;
+    r3_out=5'd0;
     end
     else
     begin
-    r1_out<=r1_pos_F;
-    r2_out<=r2_pos_F;
-    r3_out<=r3_pos_F;
+    r1_out=r1_pos_F;
+    r2_out=r2_pos_F;
+    r3_out=r3_pos_F;
     end
     end
 
 always@(data_in) begin
-r3_out = r3_out - 5'd1; 
-if (r3_out==5'd0) 
+r3_out <= r3_out - 5'd1; 
+if (r3_out<=5'd0) 
 begin 
-r3_out =5'd0;
-r2_out = r2_out- 5'd1; 
-    if (r2_out == 5'd0) 
+r2_out <= r2_out- 5'd1; 
+r3_out <=5'd25;
+    if (r2_out <= 5'd0) 
     begin 
-                r2_out=5'd0;
-                r1_out = r1_out - 5'd1; 
-                if (r1_out==5'd0)begin 
-                r1_out=5'd0;
+                r2_out <= 5'd25;
+                r1_out <= r1_out - 5'd1; 
+                if (r1_out<=5'd0)begin 
+                r1_out<=5'd0;
                 end
             end
       end
        
 end
-
-    
-
 
 endmodule
